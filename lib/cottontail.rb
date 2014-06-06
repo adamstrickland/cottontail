738fdbdf2d1450c59b928f233e84c2507e9aed19
Adam Strickland <adam.strickland@gmail.com>
2014-06-06 11:04:37 +0530

    adding auto name to Cottontail::subscribe

--- lib/cottontail.rb
+++ lib/cottontail.rb
@@ -40,7 +40,7 @@ module Cottontail
   end
 
   def self.subscribe(key, handler, options = {})
-    ::Cottontail::Worker.new({key: key, consumer: handler.new}.merge(options)).start!
+    ::Cottontail::Worker.new({name: key, key: key, consumer: handler.new}.merge(options)).start!
   end
 
   def self.configure
