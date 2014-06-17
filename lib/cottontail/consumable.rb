d1d224345f7390a0f840583c8fa63a8c46e0662a
Adam Strickland <adam.strickland@gmail.com>
2014-06-17 11:43:33 -0500

    adding Consumable#handle_payload to make defining a handler a bit easier

--- lib/cottontail/consumable.rb
+++ lib/cottontail/consumable.rb
@@ -1,7 +1,12 @@
 module Cottontail
   module Consumable
     def handle_message(delivery_info, metadata, payload)
-      Cottontail.debug "HANDLING MESSAGE #{payload} of type #{metadata.content_type}"
+      # Cottontail.debug "HANDLING MESSAGE of type #{metadata.content_type}"
+      handle_payload(payload)
+    end
+
+    def handle_payload(payload)
+      Cottontail.debug "HANDLING PAYLOAD #{payload}"
     end
   end
 end
--- spec/lib/cottontail/consumable_spec.rb
+++ spec/lib/cottontail/consumable_spec.rb
@@ -6,5 +6,16 @@ describe Cottontail::Consumable do
   subject { instance }
 
   it { should respond_to :handle_message }
-  # it { should respond_to :notify }
+  it { should respond_to :handle_payload }
+
+
+  describe '#handle_message' do
+    let(:payload) { double }
+
+    before { expect(instance).to receive(:handle_payload).with(payload) { nil } }
+
+    subject { instance.handle_message(double, double, payload) }
+
+    it { should be_nil }
+  end
 end
